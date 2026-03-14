56b3a9fb1e54b6d56d70b129a17c7cfc4591b696
OlaAdams <adamsonoladipupo@gmail.com>
2026-03-14 19:47:37 +0100

    new update

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from "react-redux"
 import { store } from './store/store'
 import App from './App.jsx'
-import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-    <Analytics/>
   </Provider>
 
 )
