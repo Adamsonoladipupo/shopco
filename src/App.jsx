bcb9b04c716ec25bbaf7244d06444b4972be9cb3
OlaAdams <adamsonoladipupo@gmail.com>
2026-03-14 19:58:16 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import {RouterProvider, RouterContextProvider} from 'react-router';
 import router from './routes/router';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
